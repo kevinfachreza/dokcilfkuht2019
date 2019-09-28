b6f561c43acebff542f9f48ad89c6bb5b8007784
kevinfachreza <kevinfachreza@yahoo.com>
2019-09-29 00:09:34 +0700

    sponsor

--- laravel/resources/views/guest/components/sponsor.blade.php
+++ laravel/resources/views/guest/components/sponsor.blade.php
@@ -1,38 +1,86 @@
-{{--
-			<section class="section border-0 m-0">
-				<div class="container text-center">
-					<h4>Acara ini Disponsori oleh</h4><br>
-					<div class="row">
-						<div class="col">
-							<div class="owl-carousel owl-theme carousel-center-active-item mb-0" data-plugin-options="{'responsive': {'0': {'items': 1}, '476': {'items': 1}, '768': {'items': 5}, '992': {'items': 5}, '1200': {'items': 5}}, 'autoplay': true, 'autoplayTimeout': 3000, 'stagePadding': 0, 'dots': false, 'nav': false}">
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-1.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-2.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-3.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-4.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-5.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-6.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-4.png" alt="">
-								</div>
-								<div class="px-3">
-									<img class="img-fluid" src="img/logos/logo-2.png" alt="">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
 
---}}
+<div class="container text-center"  id="sponsor" style="padding-bottom: 50px">
+	<h4>Acara ini Disponsori oleh</h4><br>
+	<div class="row">
+		<div class="col">
+			<div class="row">
+				<div class="col-12">
+					<h6>SPONSOR</h6>
+				</div>
+
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/bri.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/dbl.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/ef.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/kartini.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/medica.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/mojo-tras.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/pp.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/primagama.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/prodia.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/togamas.jpeg')}}" >
+				</div>
+			</div>
+
+		</div>
+		<div class="col">
+			<div class="row">
+				<div class="col-12">
+					<h6>MEDIA PARTNER</h6>
+				</div>
+				<div class="col-3 text-center sponsor-item">
+				</div>
+				<div class="col-6 text-center sponsor-item">
+					<img class="lg" src="{{url('img/sponsor/radio-global-fm.jpeg')}}">
+				</div>
+				<div class="col-3 text-center sponsor-item">
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/event-campus.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/event-mhs-sby.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/event-sby.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/info-event.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/info-lomba.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/info-sby.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/jatim-media.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/radio-unesa.jpeg')}}" >
+				</div>
+				<div class="col-4 text-center sponsor-item">
+					<img class="md" src="{{url('img/sponsor/smile-island.jpeg')}}" >
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
